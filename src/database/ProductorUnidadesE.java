448c71a34a655a97ea4851e2f1aa7e9ce5686deb
Algebro <Algebro@192.168.1.39>
2018-05-23 22:43:30 +0200

    nueva unidad

--- src/database/ProductorUnidadesE.java
+++ src/database/ProductorUnidadesE.java
@@ -21,6 +21,7 @@ public class ProductorUnidadesE {
     static ArrayList<Unidad> getUnidades(){
         ArrayList<Unidad> lista = new ArrayList<>();
         lista.add(new LeonAlado());
+        lista.add(new GuilleTigre());
         return lista;
     }
     
@@ -58,5 +59,36 @@ public class ProductorUnidadesE {
         }
     
     }
+    static class GuilleTigre extends Unidad{
+        
+        public GuilleTigre() {
+            super("Guille el Tigre", "Beast11.png", "Te mira, piensa, te mata.", TipoUnidad.Bestia, 
+                    new Atributos(100,100,11,5,3,3,6,4,5));
+            this.setIAAsociada(new searchAndDestroyBasicIA());
+            this.equipar(new ProductorEquiposA.DentaduraFuerte());
+        }
+
+
+        @Override
+        public void efectoAtacar(Unidad objetivo, double tirada, boolean acierto) {
+            int vida = (int)(1.5*this.getFuerza()) - objetivo.getBlindaje();
+            int maxSana = this.getVidaInicial()-this.getVida();
+            if(vida > maxSana)
+                vida = maxSana;
+            if(vida > 0){
+                Controlador.getInstance().apCurarUnidad(this, vida);
+                Controlador.getInstance().combateActual.panel.insertarInfo(this.getNombre() + " se curó " + vida + " de salud.");
+            }
+            if(tirada > 0.6){
+                Controlador.getInstance().combateActual.panel.insertarInfo(this.getNombre() + " aumentó su fuerza de " + 
+                        this.getFuerza() + " a " + this.modFuerza(1));
+            }
+        }
+        
+        @Override
+        public Unidad getCopia() {
+            return new GuilleTigre();
+        }
     
+    }    
 }
